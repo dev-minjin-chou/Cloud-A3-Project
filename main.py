acbfb21a3dd4e5142a2b88ce97df12911d50a4c7
Minjin chou <49186771+kuntakinte777@users.noreply.github.com>
2021-06-03 21:23:10 +1000

    login page done, signup in progress

--- main.py
+++ main.py
@@ -12,36 +12,57 @@ from boto3.dynamodb.conditions import Key, Attr
 
 app = Flask(__name__)
 
+# Querying dynamo by email.
+def query_dynamo(email, dynamodb=None):
+    dynamodb = boto3.resource('dynamodb')
+    table = dynamodb.Table('forum-login')
+
+    response = table.query(
+        KeyConditionExpression=Key('email').eq(email),
+    )
+    return response['Items']
+
+
+# For creating new user
+def create_user(email, username, password, dynamodb=None):
+    dynamodb = boto3.resource('dynamodb')
+    table = dynamodb.Table('forum-login')
+    response = table.put_item(
+        Item={
+            'email': email,
+            'user_name': username,
+            'password': password
+        }
+    )
+    return True
+
+
 @app.route("/")
 def root():
-    return render_template("login.html")
+    return render_template("home.html")
 
 
 @app.route('/login', methods=["POST", "GET"])
 def login():
 
     if request.method == "POST":
-        email = request.form.get("email")
+        user_email = request.form.get("email")
         password = request.form.get("password")
         error_msg = None
 
         try: 
-            query_result1 = query(email)
-            if len(query_result1) > 0:
-                if email != query_result1[0]['email']:
-                    error_msg = 'Email or password is invalid'
+            result = query_dynamo(user_email)
+            if len(result) > 0:
+                if user_email != result[0]['email']:
+                    error_msg = 'Email is invalid'
                     return render_template('login.html', error_msg=error_msg)
-                elif password != query_result1[0]['password']:
-                    error_msg = 'Email or password is invalid'
+                elif password != result[0]['password']:
+                    error_msg = 'Password is invalid'
                     return render_template('login.html', error_msg=error_msg)
                 else:
-                    global username_login, email_login
-                    email_login = query_result1[0]['email']
-                    username_login = query_result1[0]['user_name']
-                    subscriptions = get_musics(username_login)
-                    return render_template('main.html', error_msg=error_msg, username_login=username_login, subscriptions=subscriptions)
+                    return render_template('main.html', error_msg=error_msg)
             else:
-                error_msg = 'Email or password is invalid'
+                error_msg = 'An error occured, please try again.'
                 return render_template('login.html', error_msg=error_msg)
 
         except Exception as e:
@@ -51,6 +72,14 @@ def login():
         return render_template('login.html')
 
 
+@app.route('/signup', methods=["POST", "GET"])
+def signup():
+
+    if request.method == "POST":
+        return render_template('login.html', error_msg=error_msg)
+    else:
+        return render_template('signup.html')
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080)
+    app.run(host='127.0.0.1', port=8080)
 
