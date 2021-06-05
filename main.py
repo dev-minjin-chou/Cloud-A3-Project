f786c3ff969c85dc01be66373d3639cfd37d7b3f
north23 <s3679216@student.rmit.edu.au>
2021-06-06 02:22:50 +0700

    Add debug configuration

--- main.py
+++ main.py
@@ -1,17 +1,16 @@
+import os
+
 from flask import Flask, render_template, request, redirect, url_for
 from pycognito import Cognito
 import pymongo
 from settings import Config
+import pprint
 
-app = Flask(__name__)
-
-# DocumentDB
-# CLUSTER_ENDPOINT = 'docdb-2021-06-05-13-32-53.cluster-cj6vdcmjcqfb.ap-southeast-1.docdb.amazonaws.com'
-# DB_USERNAME = 'forumsbook'
-# DB_PASSWORD = 'dNhBnvXBBE99u5'
+application = app = Flask(__name__)
 
 aws_cognito = Cognito(Config.USER_POOL_ID, Config.CLIENT_ID, username=Config.USER_POOL_NAME)
-# mongo_client = pymongo.MongoClient(CLUSTER_ENDPOINT, username=DB_USERNAME, password=DB_PASSWORD, retryWrites='false')
+# mongo_client = pymongo.MongoClient(Config.DOCUMENTDB_CLUSTER_ENDPOINT, username=Config.DOCUMENTDB_USERNAME,
+#                                    password=Config.DOCUMENTDB_PASSWORD, retryWrites='false')
 
 loggedIn_user = None
 
@@ -19,9 +18,14 @@ loggedIn_user = None
 @app.route("/")
 def root():
     # try:
-    #     db = mongo_client.forumsbook
-    #     col = db.users
-    #     col.insert_one({'hello': 'Amazon DocumentDB'})
+    # db = mongo_client.forumsbook
+    # col = db.users
+    #
+    # result = col.find()
+    #
+    # col.insert({'username': 'north'})
+    # pprint.pprint(result)
+    # col.insert_one({'hello': 'Amazon DocumentDB'})
     # except Exception as e:
     #     print('Error')
     #     print(e)
@@ -84,4 +88,6 @@ def emailVerification():
 
 
 if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=8080, debug=True)
+    if Config.DEVELOPMENT == 'true':
+        app.debug = True
+    app.run(host='127.0.0.1', port=8080)
--- settings.py
+++ settings.py
@@ -6,7 +6,12 @@ class Config:
     load_dotenv()
 
     ENV = "development"
-    DEVELOPMENT = True
+    DEVELOPMENT = os.environ.get("DEBUG")
+
     USER_POOL_ID = os.environ.get("USER_POOL_ID")
     CLIENT_ID = os.environ.get("CLIENT_ID")
     USER_POOL_NAME = os.environ.get("USER_POOL_NAME")
+
+    DOCUMENTDB_CLUSTER_ENDPOINT = os.environ.get("DOCUMENTDB_CLUSTER_ENDPOINT")
+    DOCUMENTDB_USERNAME = os.environ.get("DOCUMENTDB_USERNAME")
+    DOCUMENTDB_PASSWORD = os.environ.get("DOCUMENTDB_PASSWORD")
