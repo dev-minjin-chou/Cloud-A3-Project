c0ba2009a4e1369bd0d7e0c39d07b31300de1586
north23 <49186624+north23@users.noreply.github.com>
2021-06-05 16:29:43 +0700

    Feature/email verification (#1)
    * login page done, signup in progress
    
    * add .gitignore
    
    * update library version
    
    * add pycognito
    
    * connect signup flow with aws cognito
    
    * send verification code to email
    
    * rename signup to register
    
    * login with aws cognito
    
    * add bootstrap and jquery
    
    * format code
    
    * remove unused style
    
    Co-authored-by: Minjin chou <49186771+kuntakinte777@users.noreply.github.com>

--- main.py
+++ main.py
@@ -1,56 +1,75 @@
-from flask import Flask, render_template, request, url_for, redirect, send_file
-from decimal import Decimal
-import logging
-import json
 import boto3
-import requests
-import os
-import tempfile
-from botocore.exceptions import ClientError
-from boto3.dynamodb.conditions import Key, Attr
-
+from boto3.dynamodb.conditions import Key
+from flask import Flask, render_template, request, redirect, url_for
+from pycognito import Cognito
 
 app = Flask(__name__)
 
+USER_POOL_ID = 'ap-southeast-1_VXmFIo9H3'
+CLIENT_ID = '2rmru8n4jfrn7hri8rco7ll0nf'
+USER_POOL_NAME = 'User'
+aws_cognito = Cognito(USER_POOL_ID, CLIENT_ID, username=USER_POOL_NAME)
+loggedIn_user = None
+
+
 @app.route("/")
 def root():
-    return render_template("login.html")
+    if loggedIn_user is not None:
+        return render_template("forum.html")
+
+    return render_template("home.html")
 
 
 @app.route('/login', methods=["POST", "GET"])
 def login():
-
     if request.method == "POST":
-        email = request.form.get("email")
+        username = request.form.get("username")
         password = request.form.get("password")
-        error_msg = None
 
-        try: 
-            query_result1 = query(email)
-            if len(query_result1) > 0:
-                if email != query_result1[0]['email']:
-                    error_msg = 'Email or password is invalid'
-                    return render_template('login.html', error_msg=error_msg)
-                elif password != query_result1[0]['password']:
-                    error_msg = 'Email or password is invalid'
-                    return render_template('login.html', error_msg=error_msg)
-                else:
-                    global username_login, email_login
-                    email_login = query_result1[0]['email']
-                    username_login = query_result1[0]['user_name']
-                    subscriptions = get_musics(username_login)
-                    return render_template('main.html', error_msg=error_msg, username_login=username_login, subscriptions=subscriptions)
-            else:
-                error_msg = 'Email or password is invalid'
-                return render_template('login.html', error_msg=error_msg)
-
+        try:
+            global loggedIn_user
+            loggedIn_user = Cognito(USER_POOL_ID, CLIENT_ID, username=username)
+            loggedIn_user.authenticate(password)
+            return redirect(url_for('root'))
         except Exception as e:
             error_msg = e
-        return render_template('login.html', error_msg=error_msg)
+            return render_template('login.html', error_msg=error_msg)
     else:
         return render_template('login.html')
 
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080)
+@app.route('/register', methods=["POST", "GET"])
+def register():
+    if request.method == "POST":
+        user_email = request.form.get("email")
+        username = request.form.get("username")
+        password = request.form.get("password")
 
+        try:
+            aws_cognito.set_base_attributes(email=user_email)
+            response = aws_cognito.register(username, password)
+            print('Register response')
+            print(response)
+            return redirect(url_for('root'))
+        except Exception as e:
+            return render_template('register.html', error_msg=e)
+
+    else:
+        return render_template('register.html')
+
+
+@app.route('/email-verification', methods=["POST"])
+def emailVerification():
+    if request.method == "POST":
+        user_name = request.form.get("user_name")
+        ver_code = request.form.get("ver_code")
+
+        try:
+            aws_cognito.confirm_sign_up(ver_code, user_name)
+            return redirect(url_for('root'))
+        except Exception as e:
+            return render_template('email-verification.html', error_msg=e)
+
+
+if __name__ == '__main__':
+    app.run(host='127.0.0.1', port=8080, debug=True)
