f8b6a9699ce36970280bb5a471bb414c96d21747
north23 <s3679216@student.rmit.edu.au>
2021-06-05 15:53:54 +0700

    login with aws cognito

--- main.py
+++ main.py
@@ -1,68 +1,40 @@
 import boto3
 from boto3.dynamodb.conditions import Key
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 from pycognito import Cognito
 
 app = Flask(__name__)
 
-aws_cognito = Cognito('ap-southeast-1_VXmFIo9H3', '2rmru8n4jfrn7hri8rco7ll0nf', username='User')
-
-
-# Querying dynamo by email.
-def query_dynamo(email, dynamodb=None):
-    dynamodb = boto3.resource('dynamodb')
-    table = dynamodb.Table('forum-login')
-
-    response = table.query(
-        KeyConditionExpression=Key('email').eq(email),
-    )
-    return response['Items']
-
-
-# For creating new user
-def create_user(email, username, password, dynamodb=None):
-    dynamodb = boto3.resource('dynamodb')
-    table = dynamodb.Table('forum-login')
-    response = table.put_item(
-        Item={
-            'email': email,
-            'user_name': username,
-            'password': password
-        }
-    )
-    return True
+USER_POOL_ID = 'ap-southeast-1_VXmFIo9H3'
+CLIENT_ID = '2rmru8n4jfrn7hri8rco7ll0nf'
+USER_POOL_NAME = 'User'
+aws_cognito = Cognito(USER_POOL_ID, CLIENT_ID, username=USER_POOL_NAME)
+loggedIn_user = None
 
 
 @app.route("/")
 def root():
+    if loggedIn_user is not None:
+        return render_template("forum.html")
+
     return render_template("home.html")
 
 
 @app.route('/login', methods=["POST", "GET"])
 def login():
     if request.method == "POST":
-        user_email = request.form.get("email")
+        username = request.form.get("username")
         password = request.form.get("password")
         error_msg = None
 
         try:
-            result = query_dynamo(user_email)
-            if len(result) > 0:
-                if user_email != result[0]['email']:
-                    error_msg = 'Email is invalid'
-                    return render_template('login.html', error_msg=error_msg)
-                elif password != result[0]['password']:
-                    error_msg = 'Password is invalid'
-                    return render_template('login.html', error_msg=error_msg)
-                else:
-                    return render_template('main.html', error_msg=error_msg)
-            else:
-                error_msg = 'An error occured, please try again.'
-                return render_template('login.html', error_msg=error_msg)
-
+            global loggedIn_user
+            loggedIn_user = Cognito(USER_POOL_ID, CLIENT_ID, username=username)
+            loggedIn_user.authenticate(password)
+            return redirect(url_for('root'))
         except Exception as e:
             error_msg = e
-        return render_template('login.html', error_msg=error_msg)
+            return render_template('login.html', error_msg=error_msg)
     else:
         return render_template('login.html')
 
@@ -79,7 +51,7 @@ def register():
             response = aws_cognito.register(username, password)
             print('Register response')
             print(response)
-            return render_template('email-verification.html', email=user_email, user_name=username)
+            return redirect(url_for('root'))
         except Exception as e:
             return render_template('register.html', error_msg=e)
 
@@ -95,7 +67,7 @@ def emailVerification():
 
         try:
             aws_cognito.confirm_sign_up(ver_code, user_name)
-            return render_template('home.html')
+            return redirect(url_for('root'))
         except Exception as e:
             return render_template('email-verification.html', error_msg=e)
 
