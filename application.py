830f01c82be124f80af9964c73012d265c45fb7a
north23 <s3679216@student.rmit.edu.au>
2021-06-06 23:31:31 +0700

    change database schema

--- application.py
+++ application.py
@@ -1,12 +1,12 @@
 import datetime
-import uuid
 
 import pymongo
+from bson.objectid import ObjectId
 from flask import Flask, render_template, request, redirect, url_for
 from pycognito import Cognito
 
-from settings import Config
 from mail import MailSender
+from settings import Config
 
 application = app = Flask(__name__)
 
@@ -16,6 +16,7 @@ mongo_client = pymongo.MongoClient(Config.DB_HOST, username=Config.DB_USERNAME,
 
 loggedIn_username = None
 DATE_TIME_FORMAT = "%Y-%m-%d, %H:%M:%S"
+DB_POST_COLLECTION = 'posts'
 
 
 @app.route("/")
@@ -24,17 +25,7 @@ def root():
         return render_template("home.html")
 
     db = mongo_client.get_database(Config.DB_NAME)
-    users = db.get_collection('users').find()
-    posts = []
-    for u in users:
-        if 'posts' not in u:
-            break
-
-        for post in u['posts']:
-            posts.append(
-                {'post_id': post['id'], 'subject': post['subject'],
-                 'postedAt': post['postedAt'].strftime(DATE_TIME_FORMAT),
-                 'username': u['username']})
+    posts = list(db.get_collection(DB_POST_COLLECTION).find())
     return render_template("forum.html", posts=posts)
 
 
@@ -87,22 +78,13 @@ def register():
 
 @app.route('/create-post', methods=["POST"])
 def createPost():
-    subject = request.form.get("subject")
     message = request.form.get("message")
 
     try:
         db = mongo_client.get_database(Config.DB_NAME)
-        users = db.get_collection('users')
-        user = users.find_one({'username': loggedIn_username})
-        post = {'id': str(uuid.uuid4()), 'subject': subject, 'message': message, 'postedAt': datetime.datetime.now()}
-
-        if user is None:
-            users.insert_one({'username': loggedIn_username, 'posts': [post]})
-        else:
-            update_document = {
-                '$push': {"posts": post}
-            }
-            users.update_one({'username': loggedIn_username}, update_document)
+        posts = db.get_collection(DB_POST_COLLECTION)
+        post = {'message': message, 'postedAt': datetime.datetime.now(), 'postedBy': loggedIn_username}
+        posts.insert_one(post)
 
         return redirect(url_for('root'))
     except Exception as e:
@@ -116,13 +98,7 @@ def viewPost(username, post_id):
 
     try:
         db = mongo_client.get_database(Config.DB_NAME)
-        results = db.get_collection('users').find_one({'username': username},
-                                                      {'posts': {
-                                                          "$elemMatch": {
-                                                              "id": post_id
-                                                          }
-                                                      }})
-        post = results['posts'][0]
+        post = db.get_collection(DB_POST_COLLECTION).find_one({'_id': ObjectId(post_id)})
         post['postedBy'] = username
         return render_template('post.html', post=post)
     except Exception as e:
@@ -131,22 +107,25 @@ def viewPost(username, post_id):
 
 @app.route('/post', methods=["POST"])
 def likePost():
-    try:
-        posted_by = request.form.get("postedBy")
-        db = mongo_client.get_database(Config.DB_NAME)
-        user = db.get_collection('users').find_one({'username': posted_by})
+    return redirect(url_for('root'))
 
-        if 'email' not in user:
-            err_msg = 'This user has not been verified with email'
-            print(err_msg)
-            return render_template('post.html', error_msg=err_msg)
-
-        email = user['email']
-        mailSender = MailSender(Config.SENDER_EMAIL)
-        mailSender.sendMail(f'{loggedIn_username} just liked your post', email)
-        return redirect(url_for('root'))
-    except Exception as e:
-        return render_template('post.html', error_msg=e)
+    # TODO: Get user email
+    # try:
+    #     posted_by = request.form.get("postedBy")
+    #     db = mongo_client.get_database(Config.DB_NAME)
+    #     user = db.get_collection('users').find_one({'username': posted_by})
+    #
+    #     if 'email' not in user:
+    #         err_msg = 'This user has not been verified with email'
+    #         print(err_msg)
+    #         return render_template('post.html', error_msg=err_msg)
+    #
+    #     destination_email = user['email']
+    #     mailSender = MailSender(Config.SENDER_EMAIL)
+    #     mailSender.sendMail(f'{loggedIn_username} just liked your post', destination_email)
+    #     return redirect(url_for('root'))
+    # except Exception as e:
+    #     return render_template('post.html', error_msg=e)
 
 
 @app.route('/email-verification', methods=["POST"])
