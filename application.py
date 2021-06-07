c834a42f5f8414f5fb97cc4b51c096f5ec1211da
north23 <49186624+north23@users.noreply.github.com>
2021-06-07 19:56:04 +0700

    Feature/email notification (#4)

--- application.py
+++ application.py
@@ -1,39 +1,33 @@
-import os
+import datetime
 
+import pymongo
+from bson.objectid import ObjectId
 from flask import Flask, render_template, request, redirect, url_for
 from pycognito import Cognito
-import pymongo
+
+from mail import MailSender
 from settings import Config
-import pprint
 
 application = app = Flask(__name__)
 
 aws_cognito = Cognito(Config.USER_POOL_ID, Config.CLIENT_ID, username=Config.USER_POOL_NAME)
-# mongo_client = pymongo.MongoClient(Config.DOCUMENTDB_CLUSTER_ENDPOINT, username=Config.DOCUMENTDB_USERNAME,
-#                                    password=Config.DOCUMENTDB_PASSWORD, retryWrites='false')
+mongo_client = pymongo.MongoClient(Config.DB_HOST, username=Config.DB_USERNAME,
+                                   password=Config.DB_PASSWORD, retryWrites='false')
 
+loggedIn_username = None
 loggedIn_user = None
+DATE_TIME_FORMAT = "%Y-%m-%d, %H:%M:%S"
+DB_POST_COLLECTION = 'posts'
 
 
 @app.route("/")
 def root():
-    # try:
-    # db = mongo_client.forumsbook
-    # col = db.users
-    #
-    # result = col.find()
-    #
-    # col.insert({'username': 'north'})
-    # pprint.pprint(result)
-    # col.insert_one({'hello': 'Amazon DocumentDB'})
-    # except Exception as e:
-    #     print('Error')
-    #     print(e)
+    if loggedIn_username is None:
+        return render_template("home.html")
 
-    if loggedIn_user is not None:
-        return render_template("forum.html")
-
-    return render_template("home.html")
+    db = mongo_client.get_database(Config.DB_NAME)
+    posts = list(db.get_collection(DB_POST_COLLECTION).find())
+    return render_template("forum.html", posts=posts)
 
 
 @app.route('/login', methods=["POST", "GET"])
@@ -46,6 +40,10 @@ def login():
             global loggedIn_user
             loggedIn_user = Cognito(Config.USER_POOL_ID, Config.CLIENT_ID, username=username)
             loggedIn_user.authenticate(password)
+
+            global loggedIn_username
+            loggedIn_username = username
+
             return redirect(url_for('root'))
         except Exception as e:
             error_msg = e
@@ -66,23 +64,102 @@ def register():
             response = aws_cognito.register(username, password)
             print('Register response')
             print(response)
-            return render_template('email-verification.html', email=user_email)
         except Exception as e:
             return render_template('register.html', error_msg=e)
 
+        try:
+            db = mongo_client.get_database(Config.DB_NAME)
+            db.get_collection('users').insert_one({'username': username, 'email': user_email})
+        except Exception as e:
+            return render_template('register.html', error_msg=e)
+
+        return render_template('email-verification.html', email=user_email)
     else:
         return render_template('register.html')
 
 
+@app.route('/logout', methods=["GET"])
+def logout():
+    global loggedIn_user
+    user = vars(loggedIn_user)
+    id_token = user['id_token']
+    refresh_token = user['refresh_token']
+    access_token = user['access_token']
+    cognito = Cognito(Config.USER_POOL_ID, Config.CLIENT_ID,
+                      id_token=id_token, refresh_token=refresh_token,
+                      access_token=access_token)
+    cognito.logout()
+    global loggedIn_username
+    loggedIn_username = None
+
+    return redirect(url_for('root'))
+
+
+@app.route('/create-post', methods=["POST"])
+def createPost():
+    message = request.form.get("message")
+
+    try:
+        db = mongo_client.get_database(Config.DB_NAME)
+        posts = db.get_collection(DB_POST_COLLECTION)
+        post = {'message': message, 'postedAt': datetime.datetime.now(), 'postedBy': loggedIn_username}
+        posts.insert_one(post)
+
+        return redirect(url_for('root'))
+    except Exception as e:
+        return render_template('forum.html', error_msg=e)
+
+
+@app.route('/users/<string:username>/posts/<string:post_id>', methods=["GET"])
+def viewPost(username, post_id):
+    if request.method == "POST":
+        return redirect(url_for('root'))
+
+    try:
+        db = mongo_client.get_database(Config.DB_NAME)
+        post = db.get_collection(DB_POST_COLLECTION).find_one({'_id': ObjectId(post_id)})
+        post['postedBy'] = username
+        return render_template('post.html', post=post)
+    except Exception as e:
+        return render_template('post.html', error_msg=e)
+
+
+@app.route('/post', methods=["POST"])
+def likePost():
+    return redirect(url_for('root'))
+
+    # TODO: Get user email
+    # try:
+    #     posted_by = request.form.get("postedBy")
+    #     db = mongo_client.get_database(Config.DB_NAME)
+    #     user = db.get_collection('users').find_one({'username': posted_by})
+    #
+    #     if 'email' not in user:
+    #         err_msg = 'This user has not been verified with email'
+    #         print(err_msg)
+    #         return render_template('post.html', error_msg=err_msg)
+    #
+    #     destination_email = user['email']
+    #     mailSender = MailSender(Config.SENDER_EMAIL)
+    #     mailSender.sendMail(f'{loggedIn_username} just liked your post', destination_email)
+    #     return redirect(url_for('root'))
+    # except Exception as e:
+    #     return render_template('post.html', error_msg=e)
+
+
 @app.route('/email-verification', methods=["POST"])
-def emailVerification():
+def verifyEmail():
     if request.method == "POST":
         username = request.form.get("username")
         ver_code = request.form.get("ver_code")
 
         try:
             aws_cognito.confirm_sign_up(ver_code, username)
-            return redirect(url_for('login'))
+
+            global loggedIn_username
+            loggedIn_username = username
+
+            return redirect(url_for('root'))
         except Exception as e:
             return render_template('email-verification.html', error_msg=e)
 
---
+++ mail.py
@@ -0,0 +1,74 @@
+import boto3
+from botocore.exceptions import ClientError
+
+
+class MailSender:
+    # Specify a configuration set. If you do not want to use a configuration
+    # set, comment the following variable, and the
+    # ConfigurationSetName=CONFIGURATION_SET argument below.
+    # CONFIGURATION_SET = "ConfigSet"
+
+    # If necessary, replace us-west-2 with the AWS Region you're using for Amazon SES.
+    AWS_REGION = "ap-southeast-1"
+
+    # The email body for recipients with non-HTML email clients.
+    BODY_TEXT = ("Amazon SES Test (Python)\r\n"
+                 "This email was sent with Amazon SES using the "
+                 "AWS SDK for Python (Boto)."
+                 )
+
+    # The HTML body of the email.
+    BODY_HTML = """<html>
+    <head></head>
+    <body>
+      <h1>Someone just liked your post, check it out!</h1>
+    </body>
+    </html>
+                """
+
+    # The character encoding for the email.
+    CHARSET = "UTF-8"
+
+    # Create a new SES resource and specify a region.
+    client = boto3.client('ses', region_name=AWS_REGION)
+
+    def __init__(self, sender_email):
+        self.sender = sender_email
+
+    def sendMail(self, subject='', destination=''):
+        # Try to send the email.
+        try:
+            # Provide the contents of the email.
+            response = self.client.send_email(
+                Destination={
+                    'ToAddresses': [
+                        destination,
+                    ],
+                },
+                Message={
+                    'Body': {
+                        'Html': {
+                            'Charset': self.CHARSET,
+                            'Data': self.BODY_HTML,
+                        },
+                        'Text': {
+                            'Charset': self.CHARSET,
+                            'Data': self.BODY_TEXT,
+                        },
+                    },
+                    'Subject': {
+                        'Charset': self.CHARSET,
+                        'Data': subject,
+                    },
+                },
+                Source=self.sender,
+                # If you are not using a configuration set, comment or delete the
+                # following line
+                # ConfigurationSetName=self.CONFIGURATION_SET,
+            )
+        # Display an error if something goes wrong.
+        except ClientError as e:
+            print(e.response['Error']['Message'])
+        else:
+            print("Email sent! Message ID:"),
+            print(response['MessageId'])
